6615f6b71d77ff1dbb4c6f9b823dd9154dbacb55
Marius Schmitt <schmittma@elektronikschule.de>
2024-06-08 19:43:56 +0200

    Rename and add more hills

--- assets/js/random_hill.js
+++ assets/js/random_hill.js
@@ -1,12 +1,6 @@
-const images = [
-    'res/mountains001.jpg',
-    'res/mountains002.jpg',
-    'res/mountains003.jpg',
-];
-
 function getRandomImage() {
-    const randomIndex = Math.floor(Math.random() * images.length);
-    return images[randomIndex];
+    const randomIndex = Math.floor(Math.random() * 9);
+    return 'res/mountains' + randomIndex + '.jpg';
 }
 
 document.body.style.backgroundImage = `url('${getRandomImage()}')`;
