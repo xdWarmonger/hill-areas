2ea887a8c1c903cf7068b9c6f904345321b9a4f5
Marius Schmitt <schmittma@elektronikschule.de>
2024-08-31 19:58:37 +0200

    Address Issues outlined in PR-1

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,26 +1,12 @@
 // Wait for the DOM to be fully loaded before executing any code
 document.addEventListener("DOMContentLoaded", function() {
-    // Create a new XMLHttpRequest object
-    var xhr = new XMLHttpRequest();
-
-    // Define the path to the header HTML file
-    var headerPath = "/pages/header.html";
-
-    // Open a GET request to fetch the header HTML file
-    xhr.open("GET", headerPath);
-
-    // Define the onload function
-    xhr.onload = function() {
-        // Check if the request was successful (status code 200)
-        if (xhr.status === 200) {
-            // Get the response text (HTML content)
-            var headerContent = xhr.responseText;
-
+    fetch('/assets/html/header.html')
+        .then(response => response.text())
+        .then(headerContent => {
             // Insert the header HTML content into the corresponding div
             document.getElementById("header").innerHTML = headerContent;
-        }
-    };
-
-    // Send the request
-    xhr.send();
+        })
+        .catch(error => {
+            console.error('Error fetching quotes:', error);
+        });
 });
